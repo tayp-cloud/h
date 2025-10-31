e755397ffb84d77391dc068c019534b45df1c7e9
Taylor Evanson <dharkflower@gmail.com>
2025-10-31 14:24:15 -0600

    Fix server to serve image files

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ const server = require('http')
       res.end();
       return;
     }
+    
+    if (req.url.startsWith('/img/')) {
+      const imgPath = path.join(__dirname, req.url);
+      if (fs.existsSync(imgPath)) {
+        res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+        fs.createReadStream(imgPath).pipe(res);
+        return;
+      }
+    }
+    
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end(slideshow);
   })
